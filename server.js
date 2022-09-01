1afe6530bb8510a70aae726ecf954af53a65ec24
DEVELOPER JETORBIT <jet-dev@MacBook-Pro-DEVELOPER.local>
2022-09-02 06:12:02 +0700

    handle not found endpoint

--- server.js
+++ server.js
@@ -4,7 +4,14 @@ const app = express();
 
 const routes = require("./routes");
 
+const notFound = (req, res, next) => {
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify({ error: 'not found' }));
+  res.status(404);
+};
+
 app.use("/", routes);
+app.use(notFound);
 
 app.listen(4000, function() {
     console.log('Listening on Port:3000');
