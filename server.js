fc2d0bec7344ca2d981adac4b19aea7cf5bab66a
DEVELOPER JETORBIT <jet-dev@MacBook-Pro-DEVELOPER.local>
2022-09-02 05:58:28 +0700

    server init

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log('halo')
+const express = require('express');
+
+const app = express();
+
+const routes = require("./routes");
+
+app.use("/", routes);
+
+app.listen(4000, function() {
+    console.log('Listening on Port:3000');
+});
